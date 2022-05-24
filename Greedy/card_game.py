997d4fe600ac63000dcdfbbce5adc6eb50357c6f
baekgdev <>
2022-05-24 06:19:02 +0000

    Update card_game.py

--- Greedy/card_game.py
+++ Greedy/card_game.py
@@ -2,9 +2,25 @@
 
 n, m = map(int, input().split())
 
+#min() 함수 사용
+"""
 result = 0
 for i in range(n):
   data = list(map(int, input().split()))
   result = max(result, min(data))
+
+print(result)
+"""
+
+#이중반복문
+result = 0
+for i in range(n):
+  data = list(map(int, input().split()))
+  
+  min_num = 10001
+  for num in data:
+    min_num = min(min_num, num)
+    
+  result = max(result, min_num)
 
 print(result)
