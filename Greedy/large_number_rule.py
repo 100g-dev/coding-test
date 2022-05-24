2d55197ace3ac1aa2db224dc1e94943b9e55114c
baekgdev <>
2022-05-24 06:03:58 +0000

    Update large_number_rule.py

--- Greedy/large_number_rule.py
+++ Greedy/large_number_rule.py
@@ -8,16 +8,7 @@ first = data[n-1]
 second = data[n-2]
 
 result = 0
+set = m // (k+1)
 
-while True:
-  for i in range(k):
-    if m == 0:
-      break
-    result += first
-    m -= 1
-  if m == 0:
-    break
-  result += second
-  m -= 1
-
+result = set*(k*first+second) + m%(k+1)*first
 print(result)
