601f4038a086d0bfe9cd3341711539d5cb52b83f
baekgdev <>
2022-05-25 09:10:15 +0000

    Update selection_sort.py

--- Sort/selection_sort.py
+++ Sort/selection_sort.py
@@ -2,11 +2,13 @@
 
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(array)):
-  min_index = i
-  for j in range(i+1, len(array)):
-    if array[min_index] > array[j]:
-      min_index = j
-  array[i], array[min_index] = array[min_index], array[i]
+def selection_sort(array):
+  for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+      if array[min_index] > array[j]:
+        min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+  return array
 
-print(array)
+print(selection_sort(array))
