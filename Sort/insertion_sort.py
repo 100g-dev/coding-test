bda6778f9b985afef9416721bb9fd0181aa8520f
baekgdev <>
2022-05-25 09:09:35 +0000

    Update insertion_sort.py

--- Sort/insertion_sort.py
+++ Sort/insertion_sort.py
@@ -2,11 +2,13 @@
 
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(1, len(array)):
-  for j in range(i, 0, -1):
-    if array[j-1] > array[j]:
-      array[j-1], array[j] = array[j], array[j-1]
-    else:
-      break
-      
-print(array)
+def insertion_sort(array):
+  for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+      if array[j-1] > array[j]:
+        array[j-1], array[j] = array[j], array[j-1]
+      else:
+        break
+  return array
+  
+print(insertion_sort(array))
