9191570ced914e2dba8ec659878f4c6367caa6c4
백지dev <baekg.dev@gmail.com>
2022-06-13 15:32:07 +0900

    Create 10870.py

---
+++ BOJ/Dynamic_programming_1/10870.py
@@ -0,0 +1,14 @@
+#10870 - 피보나치 수 5
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = [0]*21 #0<=n<20
+
+d[0]=0
+d[1]=1
+
+for i in range(2, n+1):
+    d[i]=d[i-1]+d[i-2]
+
+print(d[n])
