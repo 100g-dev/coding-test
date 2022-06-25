9cf8bf49589eadfbc4ab19d51b790af4913f98fc
백지dev <baekg.dev@gmail.com>
2022-06-25 13:36:36 +0900

    Update 18312.py

--- BOJ/Brute_force/18312.py
+++ BOJ/Brute_force/18312.py
@@ -3,12 +3,19 @@
 n, k = map(int, input().split())
 
 count = (n+1)*60*60
+
 for h in range(0, n+1):
+    if h<10: h='0'+str(h)
+    else: h=str(h)
     for m in range(0, 60):
+        if m<10: m='0'+str(m)
+        else: m=str(m)
         for s in range(0, 60):
-            tmp = []
-            tmp = list(str(h)+str(m)+str(s))
-            print(tmp)
+            if s<10: s='0'+str(s)
+            else: s=str(s)
+            
+            tmp = list(h+m+s)
+            
             if str(k) not in tmp:
                 count -= 1
 
