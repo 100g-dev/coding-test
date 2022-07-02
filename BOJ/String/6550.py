39c21594aa6de9f0fb92dd47469d22eb7cbddb23
백지dev <baekg.dev@gmail.com>
2022-07-02 12:33:35 +0900

    Create 6550.py

---
+++ BOJ/String/6550.py
@@ -0,0 +1,19 @@
+#6550 - 부분 문자열
+
+while True:
+    s, t = input().split()
+    flag = False
+
+    index = 0
+    for i in range(len(t)):
+        if s[index]==t[i]:
+            index+=1
+
+            if index==len(s):
+                flag = True
+                break
+
+    if flag:
+        print("Yes")
+    else:
+        print("No")
