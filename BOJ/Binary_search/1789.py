d18a28cf23aacca255ef5db76b1c17714c977b82
백지dev <baekg.dev@gmail.com>
2022-07-03 11:11:35 +0900

    Update 1789.py

--- BOJ/Binary_search/1789.py
+++ BOJ/Binary_search/1789.py
@@ -2,14 +2,16 @@
 
 s = int(input())
 
+start = 1
+end = s
 result = 0
-sum = 0
-for i in range(1, s+1):
-    sum += i
-    result += 1
-    
-    if sum > s:
-        result -= 1
-        break
-    
+
+while start<=end:
+    mid = (start+end)//2
+
+    if mid*(mid+1)//2 <= s:
+        result = mid
+        start = mid+1
+    else:
+        end = mid - 1
 print(result)
